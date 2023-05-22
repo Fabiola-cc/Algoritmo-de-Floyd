29a16db6358ee7e7b5657048c2ecfba45e7639c2
Maria-Villafuerte <100331510+Maria-Villafuerte@users.noreply.github.com>
2023-05-22 14:39:22 -0600

    Métodos de Cálculos

--- Calculos.java
+++ Calculos.java
@@ -1,2 +1,20 @@
 public class Calculos {
+    public void mostrar_ruta(){
+
+    }
+    public void calcular_centro(){
+
+    }
+    public void modificar_grafo(){
+
+    }
+    public void Interrupcion (){
+
+    }
+    public void Agregar_lazo(){
+
+    }
+    public void Cambiar_clima(){
+
+    }
 }
